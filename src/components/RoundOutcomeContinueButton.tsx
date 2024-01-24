00ba06b869728e0e60df9d78d7f3eb63a65bca52
adsuth <adamsutherlandam@gmail.com>
2024-01-24 18:14:26 +0000

    fixed round interim

--- src/components/RoundOutcomeContinueButton.tsx
+++ src/components/RoundOutcomeContinueButton.tsx
@@ -11,27 +11,22 @@ export default function RoundOutcomeContinueButton() {
   const [ roundNumber, setRoundNumber ] = useAtom( roundNumberAtom )
   const [ totalRoundCount, setTotalRoundCount ] = useAtom( gameTotalRoundCountAtom )
   
-  const [ isActive, setIsActive ] = useState( false )
-
   useEffect( () => {
     window.addEventListener( "keydown", pressEnterToNextRound )
     return () => window.removeEventListener( "keydown", pressEnterToNextRound )
   }, [] )
 
-  useEffect( () => {
-    setIsActive( roundOutcomeState === RoundOutcomeState.ONGOING )
-  }, [ roundOutcomeState ] )
-
   function pressEnterToNextRound( e: any )
   {
     if ( e.code !== "Enter" ) return
     if ( roundOutcomeState === RoundOutcomeState.ONGOING ) return
+    return
+    // todo: fix this 
     nextAction()
   }
 
   function nextAction() 
   {
-    if ( !isActive ) return
     if ( roundNumber === totalRoundCount ) setGameState( GameState.ENDED )
     else setRoundOutcomeState( RoundOutcomeState.ONGOING )
   }
